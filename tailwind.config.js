56cab49ad73f7b9dd590d81e20df96aeb7a4a9b1
RAHUL <rahulnaval157821@gmail.com>
2025-07-24 23:44:54 +0530

    adding classes to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { setBackgroundColorAsync } = require("expo-system-ui");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   // NOTE: Update this to include the paths to all files that contain Nativewind classes.
@@ -7,6 +9,16 @@ module.exports = {
     extend: {
       colors: {
         primary: "#030014",
+        secondary: "#151312",
+        light: {
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark: {
+          100: "#221F3D",
+          200: "#0F0D23",
+        },
         accent: "#AB8BFF",
       },
     },
